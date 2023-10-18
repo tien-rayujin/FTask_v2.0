7b9d9a61c53bce80917258c52ab7d670cbb4c9fe
ダーク Rayujin <tien.rayujin.145@gmail.com>
2023-10-18 21:40:24 +0700

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <div v-for="item in lecturerList" :key="item.id">
-    {{ item }}
-  </div>
   <RouterView />
 </template>
 
-<script setup lang="ts">
-  import store from './store'
-  import { type Lecturer } from './store/admin_store'
-  store.dispatch('fetchLecturers')
+<script setup lang="ts"></script>
 
-  const lecturerList: Array<Lecturer> = await store.getters.getLecturerList
-</script>
-
-<style scoped>
+<style lang="scss">
   @import 'scss/main.scss';
 </style>
---
+++ src/pages/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn bg-blue-400 px-5 py-3 rounded-xl text-white">
+    Hello, Click me!
+  </button>
+  <div>
+    <h1>Test Icon</h1>
+    <h3>Entypo</h3>
+
+    <span class="entypo-home"> </span>
+    <span class="entypo-air"> </span>
+    <span class="entypo-user-add"> </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
