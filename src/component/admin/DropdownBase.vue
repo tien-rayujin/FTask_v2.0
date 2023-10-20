50d4cdf49647b2d868b869e460062bd11aed1f86
ダーク Rayujin <tien.rayujin.145@gmail.com>
2023-10-20 15:45:29 +0700

    modified tag id

--- src/component/admin/DropdownBase.vue
+++ src/component/admin/DropdownBase.vue
@@ -2,9 +2,9 @@
   <div class="relative inline-block text-left px-5 group">
     <div>
       <button
+        id="menu-button"
         type="button"
         class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        id="menu-button"
         aria-expanded="true"
         aria-haspopup="true"
       >
--- src/component/admin/ModalBase.vue
+++ src/component/admin/ModalBase.vue
@@ -38,8 +38,8 @@
             </div>
             <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
               <h3
-                class="text-base font-semibold leading-6 text-gray-900"
                 id="modal-title"
+                class="text-base font-semibold leading-6 text-gray-900"
               >
                 Deactivate account
               </h3>
